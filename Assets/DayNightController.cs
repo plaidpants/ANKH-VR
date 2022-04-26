a888b30bd1b8700d99811d33f8552aec29fd5819
JWS <james.surine@gmail.com>
2022-04-26 17:29:41 -0500

    More day and night and stars

--- Assets/DayNightController.cs
+++ Assets/DayNightController.cs
@@ -17,22 +17,23 @@ public class DayNightController : MonoBehaviour
     [SerializeField] private Transform starsTransform;
     [SerializeField] private Vector3 hmsStarsLight = new Vector3(19f, 30f, 0f), hmsStarsExtinguish = new Vector3(03f, 30f, 0f);
     [SerializeField] private float starsFadeInTime = 7200f, starsFadeOutTime = 7200f;
-
+    [SerializeField] public float AtmosphereAdjustment = 0.5f;
+    [SerializeField] public float FogAdjustment = 0.007f;
     [NonSerialized] public float time;
 
     private float intensity, rotation, prev_rotation = -1f, sunSet, sunRise, sunDayRatio, fade, timeLight, timeExtinguish;
     private Color tintColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
-    private Vector3 dir;
-    private Renderer rend;
+    private Vector3 direction;
+    private Renderer starParticles;
 
     void Start()
     {
-        rend = starsTransform.GetComponent<ParticleSystem>().GetComponent<Renderer>();
+        starParticles = starsTransform.GetComponent<ParticleSystem>().GetComponent<Renderer>();
         time = HMS_to_Time(hourMinuteSecond.x, hourMinuteSecond.y, hourMinuteSecond.z);
         sunSet = HMS_to_Time(hmsSunSet.x, hmsSunSet.y, hmsSunSet.z);
         sunRise = 86400f - sunSet;
         sunDayRatio = (sunSet - sunRise) / 43200f;
-        dir = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleAtNoon), Mathf.Sin(Mathf.Deg2Rad * angleAtNoon), 0f);
+        direction = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleAtNoon), Mathf.Sin(Mathf.Deg2Rad * angleAtNoon), 0f);
         //dir = new Vector3(1f, 0f, 0f);
         starsFadeInTime /= speed;
         starsFadeOutTime /= speed;
@@ -51,40 +52,66 @@ public class DayNightController : MonoBehaviour
         }
         if (prev_rotation == -1f)
         {
-            sunTransform.eulerAngles = Vector3.zero;
+            //sunTransform.eulerAngles = Vector3.zero;
             prev_rotation = 0f;
         }
-        else prev_rotation = rotation;
+        else
+        {
+            prev_rotation = rotation;
+        }
 
         rotation = (time - 21600f) / 86400f * 360f;
-        sunTransform.Rotate(dir, rotation - prev_rotation);
-        starsTransform.Rotate(dir, rotation - prev_rotation);
+        sunTransform.Rotate(direction, rotation - prev_rotation);
+        starsTransform.Rotate(direction, rotation - prev_rotation);
 
-        if (time < sunRise) intensity = intensityAtSunSet * time / sunRise;
-        else if (time < 43200f) intensity = intensityAtSunSet + (intensityAtNoon - intensityAtSunSet) * (time - sunRise) / (43200f - sunRise);
-        else if (time < sunSet) intensity = intensityAtNoon - (intensityAtNoon - intensityAtSunSet) * (time - 43200f) / (sunSet - 43200f);
-        else intensity = intensityAtSunSet - (1f - intensityAtSunSet) * (time - sunSet) / (86400f - sunSet);
+        if (time < sunRise)
+        {
+            intensity = intensityAtSunSet * time / sunRise;
+        }
+        else if (time < 43200f)
+        {
+            intensity = intensityAtSunSet + (intensityAtNoon - intensityAtSunSet) * (time - sunRise) / (43200f - sunRise);
+        }
+        else if (time < sunSet)
+        {
+            intensity = intensityAtNoon - (intensityAtNoon - intensityAtSunSet) * (time - 43200f) / (sunSet - 43200f);
+        }
+        else
+        {
+            intensity = intensityAtSunSet - (1f - intensityAtSunSet) * (time - sunSet) / (86400f - sunSet);
+        }
 
         RenderSettings.fogColor = Color.Lerp(fogColorNight, fogColorDay, intensity * intensity);
-        if (sun != null) sun.intensity = intensity;
+        if (sun != null)
+        {
+            sun.intensity = intensity;
+        }
 
-        if (Time_Falls_Between(time, timeLight, timeExtinguish))
+        if (Time_Falls_Between(time, timeLight, timeExtinguish) == true)
         {
             fade += Time.deltaTime / starsFadeInTime;
-            if (fade > 1f) fade = 1f;
+            if (fade > 1f)
+            {
+                fade = 1f;
+            }
         }
         else
         {
             fade -= Time.deltaTime / starsFadeOutTime;
-            if (fade < 0f) fade = 0f;
+            if (fade < 0f)
+            {
+                fade = 0f;
+            }
         }
         tintColor.a = fade;
-        rend.material.SetColor("_TintColor", tintColor);
+        starParticles.material.SetColor("_TintColor", tintColor);
 
+        // turn the atmosphere off at night
         RenderSettings.skybox.SetFloat("_AtmosphereThickness", AtmosphereAdjustment * (1f - fade));
-    }
 
-    public float AtmosphereAdjustment = 0.5f;
+        // turn the fog off at night
+        RenderSettings.fogDensity = FogAdjustment * (1f - fade);
+    }
 
     private float HMS_to_Time(float hour, float minute, float second)
     {
@@ -95,15 +122,25 @@ public class DayNightController : MonoBehaviour
     {
         if (startTime<endTime)
         {
-            if (currentTime >= startTime && currentTime <= endTime) return true;
-            else return false;
+            if (currentTime >= startTime && currentTime <= endTime)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else
         {
-            if (currentTime < startTime && currentTime > endTime) return false;
-            else return true;
+            if (currentTime < startTime && currentTime > endTime)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
-        
     }
-
 }
